cd803f454a0f692f286a84c3e633a54620bfc3ed
Shishura <murilotoledo2015@gmail.com>
2025-09-15 16:37:33 -0300

    bugfix

--- main.ts
+++ main.ts
@@ -1,16 +1,20 @@
 
 function greetUser(name?: string) {
-  console.log("Olá, " + name.toUpperCase());
+  if (name) {
+    console.log("Olá, " + name.toUpperCase());
+  } else {
+    console.log("Olá, visitante!");
+  }
 }
 
 
 function calculateYearsUntilRetirement(age: string): number {
-  return 65 - age;
+  return 65 - Number(age);
 }
 
 
 function getRandomMessage(): string {
-  return Math.random() * 100;
+  return "Seu número aleatório é: " + (Math.random() * 100).toFixed(2);
 }
 
 

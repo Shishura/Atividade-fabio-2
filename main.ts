25a4107aacfbd3cddd7cb6d778586e38ee205631
Shishura <murilotoledo2015@gmail.com>
2025-09-15 15:56:47 -0300

    first commit

---
+++ main.ts
@@ -0,0 +1,19 @@
+
+function greetUser(name?: string) {
+  console.log("Olá, " + name.toUpperCase());
+}
+
+
+function calculateYearsUntilRetirement(age: string): number {
+  return 65 - age;
+}
+
+
+function getRandomMessage(): string {
+  return Math.random() * 100;
+}
+
+
+greetUser(); 
+console.log(calculateYearsUntilRetirement("30"));
+console.log(getRandomMessage()); 
